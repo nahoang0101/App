0347765d9b003dda63331e00e9bfae4c2031eb38
Henry Nguyen <nahoang9698@gmail.com>
2023-03-22 14:10:18 +0700

    wip

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import * as Environment from './libs/Environment/Environment';
 import {WindowDimensionsProvider} from './components/withWindowDimensions';
 import {KeyboardStateProvider} from './components/withKeyboardState';
 
-// For easier debugging and development, when we are in web we expose Onyx to the window, so you can more easily set data into Onyx
 if (window && Environment.isDevelopment()) {
     window.Onyx = Onyx;
 }
